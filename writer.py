fee0157f8f16a80d3f75f3a95bba4fe4e9f1dc19
sashank-tirumala <sashank@machines.run>
2024-06-20 10:31:24 -0700

    Working, always true

--- writer.py
+++ writer.py
@@ -8,21 +8,33 @@ if __name__ == "__main__":
     _dummy_array_data = np.zeros(10000)
     n = np.ones(10000)
     i = 0
-    shmseg_lidar_frame = shared_memory.SharedMemory(
-                    create=True, size=_dummy_array_data.nbytes, name="shared_test_memory"
+    shmseg_lidar_idx = shared_memory.SharedMemory(
+                    create=True, size=4, name="shared_test_memory_idx"
                 )
-    shared_memory_array = np.ndarray(_dummy_array_data.shape, dtype=np.float32, buffer=shmseg_lidar_frame.buf)
-    time_to_unlink = 10
-    start_time = time.time()
     while True:
-        shared_memory_array[:] = (n * i)[:]
+        idx = i % 2
+        if idx == 0:
+            shmseg_lidar_frame_0 = shared_memory.SharedMemory(
+                    create=True, size=_dummy_array_data.nbytes, name="shared_test_memory_2"
+                )
+            shared_memory_array = np.ndarray(_dummy_array_data.shape, dtype=np.float32, buffer=shmseg_lidar_frame_0.buf)
+            shared_memory_array[:] = (n * i)[:]
+            shmseg_lidar_idx.buf[:] = idx.to_bytes(4, byteorder='little')
+            try:
+                shmseg_lidar_frame_1.unlink()
+            except:
+                pass
+        else:
+            shmseg_lidar_frame_1 = shared_memory.SharedMemory(
+                    create=True, size=_dummy_array_data.nbytes, name="shared_test_memory_3"
+                )
+            shared_memory_array = np.ndarray(_dummy_array_data.shape, dtype=np.float32, buffer=shmseg_lidar_frame_0.buf)
+            shared_memory_array[:] = (n * i)[:]
+            shmseg_lidar_idx.buf[:] = idx.to_bytes(4, byteorder='little')
+            try:
+                shmseg_lidar_frame_0.unlink()
+            except:
+                pass
         i += 1
-        if i > 1000:
+        if i > 10001:
             i = 0
-        if time.time() - start_time > time_to_unlink:
-            print("Unlinking shared memory")
-            print(shared_memory_array)
-            shmseg_lidar_frame.close()
-            shmseg_lidar_frame.unlink()
-            break
-    pass
